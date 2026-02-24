555a6ce1562498c68cf41f0dcfcbec0b1c28d998
yassine-el-B <340524@student.mboutrecht.nl>
2026-02-24 10:38:19 +0100

    les 2 heb geleerd hoe je error berichten kunt maken met mixen van php en json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions -> renderable(function (NotFoundHttpException $e) {
+            return response() ->json(['message' => 'Resource not found',], 404);
+        });
     })->create();
